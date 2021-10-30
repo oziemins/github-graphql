4110e05a073ab2eb18ec1c77bdcd27d04482daa9
oziemins <oziemins@cisco.com>
2021-10-30 16:41:23 +0200

    add footer

---
+++ src/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+const Footer = () => (
+  <div className="Footer">
+    <div>
+      <small>
+        <span className="Footer-text">Developed by @oziemins</span>
+      </small>
+    </div>
+  </div>
+);
+
+export default Footer;
