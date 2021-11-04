6bb6f96fbc28e6ebeadccb8598a3fc2f64dc1ced
oziemins <oziemins@cisco.com>
2021-11-04 12:14:19 +0100

    loading data

--- src/Components/Repository/Repository.js
+++ src/Components/Repository/Repository.js
@@ -3,13 +3,17 @@ import React from 'react';
 const Repository = (props) => {
   console.log("props.repository", props.repository)
   console.log("length", typeof(props.repository))
-  const { name, description, isFork } = props.repository;
+  const { name, description, isFork, pushedAt, issues, pullRequests } = props.repository;
 
   return (
     <div>
       <span>Name: {name} </span> 
-      <span>Description: {description} </span> 
-      <span>{isFork ? "yes": "no"} </span> 
+      <span>Description: {description} </span>
+      <span>Is it a fork? {isFork ? "yes": "no"} </span> 
+      <span>Last commit date: {pushedAt} </span>
+      <span>Issue count: {issues.totalCount}</span>
+      <span>Pull requests count: {pullRequests.totalCount}</span>
+      <div>---------</div>
     </div>
   );
 };
--- src/Components/RepositoryList/RepositoryList.js
+++ src/Components/RepositoryList/RepositoryList.js
@@ -7,7 +7,7 @@ const RepositoryList = (props) => {
   console.log("RepositoryList props", props)
   const { data } = props
   console.log("repo data", data)
-  
+  console.log("total count", data.user.repositories.totalCount)
   //const { loading, error, data } = useQuery(QUERY_REPO);
   //console.log(data)
   //console.log(error)
@@ -26,10 +26,10 @@ const RepositoryList = (props) => {
       <h2> {data.user.login} </h2>
       <h2> {data.user.url} </h2>
       <h2> {data.user.id} </h2> */}
-      {data.user.repositories.totalCount === 0 ? <span>No repositories!</span> : null}
-      {data.user.repositories.nodes.map((repository) => (
+      {data.user.repositories.totalCount === 0 ? <span>No repositories!</span> : 
+      data.user.repositories.nodes.map((repository) => (
         <Repository key={repository.id} repository={repository} />
-      ))}
+      ))} 
     </div>
   );
 };
