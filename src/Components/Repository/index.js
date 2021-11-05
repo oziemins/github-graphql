7d32cdf79e2f2b3539a928ec92d1dbef20081f63
oziemins <oziemins@cisco.com>
2021-11-05 12:39:48 +0100

    fetchmore + css

--- src/Components/Repository/index.js
+++ src/Components/Repository/index.js
@@ -5,13 +5,17 @@ const Repository = (props) => {
   console.log("props.repository", props.repository)
   console.log("length", typeof(props.repository))
   const { name, description, isFork, url, pushedAt, issues, pullRequests } = props.repository;
+  //console.log("pushed at type", pushedAt.split("T"))
+  const pushedAtDate = pushedAt.split("T")[0]
+  const pushedAtHour = (pushedAt.split("T")[1].split("Z"))[0]
+  //console.log(pushedAtHour)
 
   return (
     <div className="repository-container">
       <div>Name: <a href={url} className="repository-link">{name}</a> </div>
       <div>Description: {{description} === null ? {description} : "No description" } </div>
       <div>Is it a fork? {isFork ? "Yes": "No"} </div> 
-      <div>Last commit date: {pushedAt} </div>
+      <div>Last commit date: {pushedAtDate} at {pushedAtHour} </div>
       <div>Issue count: {issues.totalCount}</div>
       <div>Pull requests count: {pullRequests.totalCount}</div>
     </div>
